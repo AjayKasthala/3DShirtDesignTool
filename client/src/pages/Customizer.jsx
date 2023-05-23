e7df9864aae5f940f3cba1a317367606024ebe9e
Ajay Kasthala <ajay.kasthala@gmail.com>
2023-05-23 12:47:01 +0530

    Added My Project

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://threedshirtcustomizerwithai-server.onrender.com', {
+      const response = await fetch('https://threedshirtcustomizerwithai-server.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
